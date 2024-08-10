044cd463831533646b38b7e024dd1ed5424f61a5
MariCruz <maricruzdev@gmail.com>
2024-08-10 00:49:52 -0300

    footer

--- layout/src/components/sectionResults/src/App.jsx
+++ layout/src/components/sectionResults/src/App.jsx
@@ -1,9 +1,13 @@
 import { SectionResults } from "./components/sectionResults"
 import "./App.css" 
+import { Footer } from "./components/footer"
 
 function App() { 
   return (
+    <>
     <SectionResults/>
+    <Footer/>
+    </>    
   )
 }
 export default App
---
+++ layout/src/components/sectionResults/src/components/footer.jsx
@@ -0,0 +1,10 @@
+import "./footer.css"
+
+export function Footer() {
+return (
+  <footer>
+    <img src="azulismoblack.png" alt="azulismo black" />
+    <p>&copy;2024 azulismo.com</p>
+  </footer>
+)
+}
