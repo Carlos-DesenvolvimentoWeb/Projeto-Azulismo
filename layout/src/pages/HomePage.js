a5c5dff543debfaaafbee336e19d1893d43b0dc7
lucas-sinn <lucas.lfcds@gmail.com>
2024-08-22 20:06:07 -0300

    Intro

--- layout/src/pages/HomePage.js
+++ layout/src/pages/HomePage.js
@@ -3,12 +3,14 @@ import Header from '../components/Header';
 import FilterBar from '../components/FilterBar';
 import CourseSection from '../components/CourseSection';
 import Footer from '../components/Footer';
+import Intro from '../components/Intro';  
 
 function HomePage() {
   return (
     <div className="App">
        <Header />
        <FilterBar />
+       <Intro />
        <CourseSection title="Front-End" />
        <CourseSection title="Orientação Objeto" />
        <Footer />
