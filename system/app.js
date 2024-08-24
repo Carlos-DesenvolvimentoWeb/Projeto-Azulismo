94f90fe6c46f5467793734d5673fff767ee9e833
Marcio Holanda <marcioh.dev@gmail.com>
2024-08-24 15:17:46 -0300

    Login de Wandeson, conexão com o banco de dados, incluidas novas bibliotecas.

--- system/app.js
+++ system/app.js
@@ -1,7 +1,28 @@
+//Sincronização com o banco de dados
+(async () => {
+  const database = require("../database/db")
+
+  const User = require("./models/userModel")
+  const Crianca = require("./models/criancaModel")
+
+  await database
+    .sync() //{force:true}
+    .then((result) => {
+      console.log("Banco Sincronizado!");
+    })
+    .catch((error) => {
+      console.log({ mensagem: error });
+    });
+})();
+
+
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
+const passport = require("./config/passport") //n
+const sequelize = require("../database/db")
+const User = require("./models/userModel")
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 // Configurações e middlewares aqui
@@ -10,7 +31,7 @@ app.use(express.json());
 // Rotas da API
 const userRoutes = require('./routes/userRoute');
 
-app.use('/', userRoutes);
+app.use('/api', userRoutes);
 
 app.use(express.static(path.join(__dirname, '../layout/build')));
 
