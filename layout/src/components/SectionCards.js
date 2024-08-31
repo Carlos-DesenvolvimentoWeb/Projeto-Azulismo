798590fe4dc272b753f973c11c748fecb53cc03b
MariCruz <maricruzdev@gmail.com>
2024-08-30 23:40:40 -0300

    carrossel, imagens, bg

--- layout/src/components/SectionCards.js
+++ layout/src/components/SectionCards.js
@@ -1,40 +1,49 @@
-import "./SectionCards.css"
+import "./SectionCards.css";
+import imgcards1 from "../assets/img/imgcards1.png";
+import imgcards2 from "../assets/img/imgcards2.png";
+import imgcards3 from "../assets/img/imgcards3.png";
 
 const SectionCards = () => {
-  return(
+  return (
     <section class="why">
-        <h1>Por que Azulismo?</h1>
-        <div class="cards">
-          <div class="card">
-            <h2>Experiência focada em crianças com TEA</h2>
-            <div class="content">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </div>
-          <div class="card">
-            <h2>Desenvolvimento Criativo</h2>
-            <div class="content">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </div>
-          <div class="card">
-            <h2>Gratuito</h2>
-            <div class="content">
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
+      <h1>Por que Azulismo?</h1>
+      <div class="cards">
+        <div class="card">
+          <img src={imgcards1} alt="crianças" />
+          <h2>Experiência focada em crianças com TEA</h2>
+          <div class="content">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
           </div>
         </div>
-      </section>
-  )
-}
+        <div class="card">
+          <img src={imgcards2} alt="crianças" />
+          <h2>
+            Desenvolvimento <br /> Criativo
+          </h2>
+          <div class="content">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img src={imgcards3} alt="crianças" />
+          <h2>Gratuito</h2>
+          <div class="content">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="bg"></div>
+    </section>
+  );
+};
 
 export default SectionCards;
--- layout/src/components/SectionDepoiments.js
+++ layout/src/components/SectionDepoiments.js
@@ -1,62 +1,88 @@
-import "./SectionDepoiments.css"
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./SectionDepoiments.css";
+import img1 from "../assets/img/img1.png";
+import img2 from "../assets/img/img2.png";
+import img3 from "../assets/img/img3.png";
+import Slider from "react-slick";
 
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3,
-    slidesToSlide: 1
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-  },
-};
-const SectionDepoiments = () => {
-  return(
-    <section className="dep">
-        <h2>Depoimentos de Pais dos alunos</h2>
-       <Carousel responsive={responsive} infinite={true} itemClass="cards-depoiment" containerClass="carousel-container" arrows={true}>
-                <div className="card-item">
-                <p className="depoiment">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block" }}
+      onClick={onClick}
+    />
+  );
+}
 
-              <div className="card-item">
-                <p className="depoiment">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block" }}
+      onClick={onClick}
+    />
+  );
+}
 
-              <div className="card-item">
-                <p className="depoiment">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-
-              <div className="card-item swiper-slide">
-                <p class="depoiment">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
-       </Carousel>
-       </section>
-  )
+function SectionDepoiments() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+  return (
+    <section className="depoiments">
+      <h2>Depoimentos de Pais de alunos</h2>
+      <div className="slider-container">
+        <Slider className="sliders" {...settings}>
+          <div>
+            <article>
+              <img src={img1} alt="kids" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </article>
+          </div>
+          <div>
+            <article>
+              <img src={img2} alt="kids" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </article>
+          </div>
+          <div>
+            <article>
+              <img src={img3} alt="kids" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </article>
+          </div>
+          <div>
+            <article>
+              <img src={img2} alt="kids" />
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </article>
+          </div>
+        </Slider>
+      </div>
+    </section>
+  );
 }
 
 export default SectionDepoiments;
