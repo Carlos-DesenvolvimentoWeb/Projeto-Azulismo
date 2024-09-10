6dea972bc3352e87a388bd7074ef22c5fdf296e7
MariCruz <maricruzdev@gmail.com>
2024-09-10 17:25:22 -0300

    atualizações dos componentes

---
+++ layout/src/components/SectionCourse.js
@@ -0,0 +1,49 @@
+import "./SectionCourse.css";
+import call from "../assets/img/call.png";
+import book from "../assets/img/book.svg";
+import icon from "../assets/img/icon.svg";
+import book3 from "../assets/img/book3.svg";
+
+function SectionCourse() {
+  return (
+    <section>
+      <div className="focos">
+        <h2>Quais os focos dos cursos?</h2>
+
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In accumsan
+          dignissim sollicitudin.
+        </span>
+
+        <button type="button">Comece agora!</button>
+      </div>
+      <div className="kids">
+        <img className="img-kids" src={call} alt="exemplo1" />
+      </div>
+
+      <div className="icones">
+        <button>
+          <img src={book} alt="exemplo2" />
+
+          <p>Audio Book</p>
+        </button>
+
+        <button>
+          <div className="icones-div">
+            <img src={icon} alt="exemplo3" />
+          </div>
+
+          <p>Ao vivo</p>
+        </button>
+
+        <button>
+          <img src={book3} alt="exemplo3" />
+
+          <p>Aulas Gravadas</p>
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default SectionCourse;
--- layout/src/pages/HomePage.js
+++ layout/src/pages/HomePage.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import FilterBar from '../components/FilterBar';
+import React from "react";
+import Navbar from "../components/Navbar";
 // import CourseSection from '../components/CourseSection';
-import Footer from '../components/Footer';
-import Intro from '../components/Intro';  
-import Modos from '../components/Modos';
-import SectionCards from '../components/SectionCards';
-import SectionDepoiments from '../components/SectionDepoiments';
-import { SectionResults } from '../components/SectionResults';
-
+import Footer from "../components/Footer";
+import Intro from "../components/Intro";
+import Modos from "../components/Modos";
+import SectionCards from "../components/SectionCards";
+import SectionDepoiments from "../components/SectionDepoiments";
+import { SectionResults } from "../components/SectionResults";
+import SectionCourse from "../components/SectionCourse";
 
 function HomePage() {
   return (
     <div className="App">
-       <Navbar />
-       <FilterBar />
-       <Intro />
-       <Modos />
-       {/* <CourseSection title="Front-End" />
+      <Navbar />
+
+      <Intro />
+      <Modos />
+      {/* <CourseSection title="Front-End" />
        <CourseSection title="Orientação Objeto" /> */}
-       <SectionResults />
-       <SectionCards />
-       <SectionDepoiments />
-       <Footer />
-     </div>
+      <SectionResults />
+      <SectionCards />
+      <SectionCourse />
+      <SectionDepoiments />
+      <Footer />
+    </div>
   );
 }
 
