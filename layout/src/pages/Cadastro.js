162652cfba94e94afc0da7e20f243bdc4381ce1b
Marcio Holanda <marcioh.dev@gmail.com>
2024-09-09 21:46:06 -0300

    Cadastro formulário teste

--- layout/src/pages/Cadastro.js
+++ layout/src/pages/Cadastro.js
@@ -1,17 +1,64 @@
 import React from 'react';
+import { useState, useRef } from 'react';
 import Navbar from '../components/Navbar';
 import FilterBar from '../components/FilterBar';
 import Footer from '../components/Footer';
-
+import api from '../services/api';
 
 function Cadastro() {
+  const [usuarios, setUsuarios] = useState([]);
+  const [message, setMessage] = useState('');
+
+  const inputNomeCrianca = useRef();
+  const inputEmail = useRef();
+  const inputTelefone = useRef();
+  const inputPassword = useRef();
+  const inputDataNascimentoCrianca = useRef();
+  const inputNomeResponsavel = useRef();
+
+  async function CreateUser() {
+    try {
+      const response = await api.post('/api/register', {
+        nomeCrianca: inputNomeCrianca.current.value,
+        email: inputEmail.current.value,
+        telefone: inputTelefone.current.value,
+        password: inputPassword.current.value,
+        dataNascimentoCrianca: inputDataNascimentoCrianca.current.value,
+        nomeResponsavel: inputNomeResponsavel.current.value,
+      });
+      setMessage('Usuário criado com sucesso!');
+      console.log('Usuário criado:', response.data);
+    } catch (error) {
+      if (error.response) {
+        // O servidor respondeu com um status diferente de 2xx
+        setMessage(`Erro: ${error.response.data}`); // Mensagem de erro do servidor
+        console.error('Erro na resposta:', error.response.data);
+      } else if (error.request) {
+        // A solicitação foi feita, mas nenhuma resposta foi recebida
+        setMessage('Erro na solicitação. Por favor, tente novamente.');
+        console.error('Erro na solicitação:', error.request);
+      } else {
+        // Algo aconteceu ao configurar a solicitação
+        setMessage(`Erro: ${error.message}`);
+        console.error('Erro:', error.message);
+      }
+    }
+  }
+
   return (
-    <div>
-       <Navbar />
-       <FilterBar />
-       <h1>Tela de cadastro</h1>
-       <Footer />
-     </div>
+    <div className='container'>
+      <Navbar />
+      <FilterBar />
+      <h1>Tela de cadastro</h1>
+      <input placeholder='*Nome completo' name='nome' type='text' ref={inputNomeCrianca} />
+      <input placeholder='*Seu @e-mail' name='email' type='email' ref={inputEmail} />
+      <input placeholder='(99) 99999-9999' name='telefone' type='text' ref={inputTelefone} />
+      <input placeholder='*Senha' name='senha' type='password' ref={inputPassword} />
+      <input placeholder='*Data de nascimento 2000-01-01' type='date' ref={inputDataNascimentoCrianca} />
+      <input placeholder='*Nome do responsável' type='text' ref={inputNomeResponsavel} />
+      <button type='button' onClick={CreateUser}>Cadastrar</button>
+      <Footer />
+    </div>
   );
 }
 
