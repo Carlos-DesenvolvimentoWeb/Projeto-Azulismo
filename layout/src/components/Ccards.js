c92ccb27670d815380a250fe5d6d6d6116524b99
MariCruz31 <maricruzdev@gmail.com>
2024-09-19 22:49:42 -0300

    Add mais cursos-page Dashboard

--- layout/src/components/Ccards.js
+++ layout/src/components/Ccards.js
@@ -3,11 +3,8 @@ import "./Ccards.css";
 function Ccards() {
   return (
     <section class="front">
-      {/* <div>
-        <h3>Front-End</h3> <h2>VER MAIS</h2>
-      </div> */}
       <div className="texto">
-        <h3>Front-End</h3>
+        <h3>Lógica de Programação</h3>
         <div className="frase">
           <h5>Ver mais &gt;</h5>
         </div>
@@ -57,7 +54,103 @@ function Ccards() {
       </div>
 
       <div className="texto">
-        <h3>Orientação objeto</h3>
+        <h3>Orientação Objeto</h3>
+        <div className="frase">
+          <h5>Ver mais &gt;</h5>
+        </div>
+      </div>
+      <div class="ccards">
+        <div class="ccard">
+          <h4>Lorem ipsum</h4>
+          <div class="content">
+            <p>
+              Praesent sollicitudin, libero venenatis condimentum maximus, orci
+              ipsum tincidunt orci, vitae aliquet tellus sapien in justo. Duis
+              vel enim elit. Morbi quis tempus ipsum, nec porttitor libero.
+            </p>
+          </div>
+        </div>
+        <div class="ccard">
+          <h4>Lorem ipsum</h4>
+          <div class="content">
+            <p>
+              Praesent sollicitudin, libero venenatis condimentum maximus, orci
+              ipsum tincidunt orci, vitae aliquet tellus sapien in justo. Duis
+              vel enim elit. Morbi quis tempus ipsum, nec porttitor libero.
+            </p>
+          </div>
+        </div>
+        <div class="ccard">
+          <h4>Lorem ipsum</h4>
+          <div class="content">
+            <p>
+              Praesent sollicitudin, libero venenatis condimentum maximus, orci
+              ipsum tincidunt orci, vitae aliquet tellus sapien in justo. Duis
+              vel enim elit. Morbi quis tempus ipsum, nec porttitor libero.
+            </p>
+          </div>
+        </div>
+        <div class="ccard">
+          <h4>Lorem ipsum</h4>
+          <div class="content">
+            <p>
+              Praesent sollicitudin, libero venenatis condimentum maximus, orci
+              ipsum tincidunt orci, vitae aliquet tellus sapien in justo. Duis
+              vel enim elit. Morbi quis tempus ipsum, nec porttitor libero.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="texto">
+        <h3>Algoritmos</h3>
+        <div className="frase">
+          <h5>Ver mais &gt;</h5>
+        </div>
+      </div>
+      <div class="ccards">
+        <div class="ccard">
+          <h4>Lorem ipsum</h4>
+          <div class="content">
+            <p>
+              Praesent sollicitudin, libero venenatis condimentum maximus, orci
+              ipsum tincidunt orci, vitae aliquet tellus sapien in justo. Duis
+              vel enim elit. Morbi quis tempus ipsum, nec porttitor libero.
+            </p>
+          </div>
+        </div>
+        <div class="ccard">
+          <h4>Lorem ipsum</h4>
+          <div class="content">
+            <p>
+              Praesent sollicitudin, libero venenatis condimentum maximus, orci
+              ipsum tincidunt orci, vitae aliquet tellus sapien in justo. Duis
+              vel enim elit. Morbi quis tempus ipsum, nec porttitor libero.
+            </p>
+          </div>
+        </div>
+        <div class="ccard">
+          <h4>Lorem ipsum</h4>
+          <div class="content">
+            <p>
+              Praesent sollicitudin, libero venenatis condimentum maximus, orci
+              ipsum tincidunt orci, vitae aliquet tellus sapien in justo. Duis
+              vel enim elit. Morbi quis tempus ipsum, nec porttitor libero.
+            </p>
+          </div>
+        </div>
+        <div class="ccard">
+          <h4>Lorem ipsum</h4>
+          <div class="content">
+            <p>
+              Praesent sollicitudin, libero venenatis condimentum maximus, orci
+              ipsum tincidunt orci, vitae aliquet tellus sapien in justo. Duis
+              vel enim elit. Morbi quis tempus ipsum, nec porttitor libero.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="texto">
+        <h3>Front-End</h3>
         <div className="frase">
           <h5>Ver mais &gt;</h5>
         </div>
--- layout/src/components/Navbar.js
+++ layout/src/components/Navbar.js
@@ -46,6 +46,7 @@ function Navbar() {
                 )}
               </a>
             </li>
+
             <li>
               <a href="../ComoUsar">Como usar</a>
             </li>
