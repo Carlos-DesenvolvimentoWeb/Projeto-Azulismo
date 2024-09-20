ccf1dc5dfe61ef957f0190109b861b60a5799eb5
Clara Oliveira <clara.santoss.oliveira@gmail.com>
2024-09-20 15:44:36 -0300

    CSS da filterbar, correções nos cards e na homepage, logo agora linka com página principal

--- layout/src/components/FilterBar.js
+++ layout/src/components/FilterBar.js
@@ -1,30 +1,35 @@
-import React from "react";
-import "./FilterBar.css";
+import React from 'react';
+import './FilterBar.css';
+import Lupa from "../images/lupa.svg";
 
 const FilterBar = () => {
   return (
     <div className="filter-bar">
       <div className="filters">
-        <button className="filter-button">Cursos</button>
-        <button className="filter-button1">Lógica de Programação</button>
-        <button className="filter-button1">Orientação Objeto</button>
-        <button className="filter-button">Algoritmos</button>
-        <button className="filter-button">Front-end</button>
+        <button className="filtro-button">Cursos</button>
+        <button className="filtro-button" id="tamanho-logica">Lógica de Programação</button>
+        <button className="filtro-button" id="tamanho-orientacao">Orientação Objeto</button>
+        <button className="filtro-button">Algoritmos</button>
+        <button className="filtro-button">Front-end</button>
       </div>
-      <div className="search-filter">
-        <input
-          type="text"
-          placeholder="Procurar Curso"
-          className="search-input"
-        />
-        <button className="search-button">🔍</button>
-        <select className="sort-select">
-          <option value="recent">Mais Recente</option>
-          <option value="oldest">Mais Antigo</option>
-        </select>
+      <div className="search-filtro">
+        <div className="search-container">
+          <input type="text" placeholder="Procurar Curso" className="search-input" />
+          <button className="search-button" aria-label="Buscar">
+            <img src={Lupa} alt="Buscar" width="24" height="24" />
+          </button>
+        </div>
+        <div className="filter-container">
+          <div className="filter-text">Filtrar por:</div>
+          <select className="sort-select">
+            <option value="recent">Mais Recente</option>
+            <option value="oldest">Mais Antigo</option>
+          </select>
+        </div>
       </div>
     </div>
   );
 };
 
 export default FilterBar;
+
